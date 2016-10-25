ef8c2a11c3dc3516753f039062138773bb985b7f
conanconstantine <hung.conan@gmail.com>
2016-10-25 16:56:05 +0800

    change localhost to physical ip address

--- model/db.js
+++ model/db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/nodewebappdb');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/nodewebappdb');
